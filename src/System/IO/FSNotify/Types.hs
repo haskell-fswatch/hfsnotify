563efaae12cf58b1327ed1f02c6a93265c22edaf
Mark Dittmer <mark.s.dittmer@gmail.com>
2012-06-10 23:38:31 -0400

    Added types

---
+++ src/System/IO/FSNotify/Types.hs
@@ -0,0 +1,22 @@
+--
+-- Copyright (c) 2012 Mark Dittmer - http://www.markdittmer.org
+-- Developed for a Google Summer of Code project - http://gsoc2012.markdittmer.org
+--
+{-# LANGUAGE MultiParamTypeClasses, CPP #-}
+
+module System.IO.FSNotify.Types
+       ( initSession
+       , killSession
+       , listen
+       , rlisten
+       ) where
+
+#ifdef OS_Linux
+import System.IO.FSNotify.Linux
+#else
+#ifdef OS_Win32
+import System.IO.FSNotify.Win32
+#else
+#endif
+#endif
+import System.IO.FSNotify.Polling
