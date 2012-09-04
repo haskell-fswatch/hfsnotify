f8c2e092bd9f5ec963db85749ced8a27d3a9dd93
Mark Dittmer <mark.s.dittmer@gmail.com>
2012-09-04 08:47:11 -0400

    IORef-based debouncing on Linux

--- src/System/IO/FSNotify/Linux.hs
+++ src/System/IO/FSNotify/Linux.hs
@@ -14,7 +14,10 @@ import Prelude hiding (FilePath)
 
 import Control.Concurrent.Chan
 import Control.Exception
+import Control.Monad (when)
+import Data.IORef (atomicModifyIORef, IORef, newIORef, readIORef)
 import Data.Time.Clock (UTCTime, getCurrentTime)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import Data.Typeable
 -- import Debug.Trace (trace)
 import Filesystem.Path.CurrentOS
@@ -25,8 +28,13 @@ import qualified System.INotify as INo
 
 type NativeManager = INo.INotify
 
+type IOEvent = IORef Event
+
 data EventVarietyMismatchException = EventVarietyMismatchException deriving (Show, Typeable)
 instance Exception EventVarietyMismatchException
+
+void :: IO ()
+void = return ()
 
 -- Note that INo.Closed in this context is "modified" because we listen to
 -- CloseWrite events.
@@ -38,20 +46,22 @@ fsnEvent basePath timestamp (INo.MovedIn  False       name  _) = Just (Added    
 fsnEvent basePath timestamp (INo.Deleted  False       name   ) = Just (Removed  (basePath </> (fp name)) timestamp)
 fsnEvent _        _         _                                  = Nothing
 
-handleInoEvent :: ActionPredicate -> EventChannel -> FilePath -> INo.Event -> IO ()
--- handleInoEvent _       _    basePath inoEvent | trace ("Linux: handleInoEvent " ++ show basePath ++ " " ++ show inoEvent) False = undefined
-handleInoEvent actPred chan basePath inoEvent = do
+handleInoEvent :: ActionPredicate -> EventChannel -> FilePath -> IOEvent -> INo.Event -> IO ()
+-- handleInoEvent _       _    basePath _   inoEvent | trace ("Linux: handleInoEvent " ++ show basePath ++ " " ++ show inoEvent) False = undefined
+handleInoEvent actPred chan basePath ior inoEvent = do
   currentTime <- getCurrentTime
   let maybeFsnEvent = fsnEvent basePath currentTime inoEvent
-  handleEvent actPred chan maybeFsnEvent
+  handleEvent actPred chan ior maybeFsnEvent
 
-handleEvent :: ActionPredicate -> EventChannel -> Maybe Event -> IO ()
--- handleEvent actPred _    (Just event) | trace ("Linux: handleEvent " ++ show (actPred event) ++ " " ++ show event) False = undefined
-handleEvent actPred chan (Just event)
-  | actPred event       = writeChan chan event
-  | otherwise           = return ()
--- handleEvent _ _ Nothing | trace ("Linux handleEvent Nothing") False = undefined
-handleEvent _ _ Nothing = return ()
+handleEvent :: ActionPredicate -> EventChannel -> IOEvent -> Maybe Event -> IO ()
+-- handleEvent actPred _    _   (Just event) | trace ("Linux: handleEvent " ++ show (actPred event) ++ " " ++ show event) False = undefined
+handleEvent actPred chan ior (Just event) =
+  when (actPred event) $ do
+    lastEvent <- readIORef ior
+    when (not $ debounce lastEvent event) (writeChan chan event)
+    atomicModifyIORef ior (\_ -> (event, ()))
+-- handleEvent _ _ _ Nothing | trace ("Linux handleEvent Nothing") False = undefined
+handleEvent _ _ _ Nothing = void
 
 varieties :: [INo.EventVariety]
 varieties = [INo.Create, INo.Delete, INo.MoveIn, INo.MoveOut, INo.CloseWrite]
@@ -63,10 +73,11 @@ instance FileListener INo.INotify where
 
   listen iNotify path actPred chan = do
     path' <- canonicalizeDirPath path
-    INo.addWatch iNotify varieties (encodeString path') (handler path')
-    return ()
+    ior <- newIORef (Added (fp "") (posixSecondsToUTCTime 0))
+    _ <- INo.addWatch iNotify varieties (encodeString path') (handler path' ior)
+    void
     where
-      handler :: FilePath -> INo.Event -> IO ()
+      handler :: FilePath -> IOEvent -> INo.Event -> IO ()
       handler = handleInoEvent actPred chan
 
   -- rlisten iNotify path actPred chan | trace ("Linux: rlisten " ++ fp path) False = undefined
@@ -76,13 +87,15 @@ instance FileListener INo.INotify where
     mapM_ pathHandler (path':paths)
     where
       pathHandler :: FilePath -> IO ()
-      -- pathHandler filePath | trace ("Linux: rlisten pathHandler " ++ show filePath) False = undefined
+      -- pathHandler filePath _   | trace ("Linux: rlisten pathHandler " ++ show filePath) False = undefined
       pathHandler filePath = do
-        INo.addWatch iNotify varieties (fp filePath) (handler filePath)
-        return ()
+        ior <- newIORef (Added (fp "") (posixSecondsToUTCTime 0))
+        _ <- INo.addWatch iNotify varieties (fp filePath) (handler filePath ior)
+        void
         where
-          handler :: FilePath -> INo.Event -> IO ()
-          -- handler _ event | trace ("Linux: rlisten handler " ++ show event) False = undefined
-          handler baseDir (INo.Created True dirPath) =
+          handler :: FilePath -> IOEvent -> INo.Event -> IO ()
+          -- handler _ _ event | trace ("Linux: rlisten handler " ++ show event) False = undefined
+          handler baseDir _   (INo.Created True dirPath) =
             rlisten iNotify (baseDir </> (fp dirPath)) actPred chan
-          handler baseDir event                      = handleInoEvent actPred chan baseDir event
+          handler baseDir ior event                      =
+            handleInoEvent actPred chan baseDir ior event
