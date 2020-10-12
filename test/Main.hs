1dfe8a6eafbf5c320afeefbbe377676c4adf474c
Tom McLaughlin <tom@codedown.io>
2020-10-12 14:43:32 -0600

    More test refactoring

--- test/Main.hs
+++ test/Main.hs
@@ -3,12 +3,9 @@
 module Main where
 
 import Control.Monad
-import Data.Monoid
+import EventTests
 import Prelude hiding (FilePath)
-import System.Directory
 import System.FSNotify
-import System.FilePath
-import System.IO
 import Test.Hspec
 import Util
 
@@ -23,86 +20,3 @@ main = do
     describe "ThreadPerEvent" $ eventTests ThreadPerEvent hasNative
 
     it "respects the confOnHandlerException option" $ pending
-
-eventTests :: ThreadingMode -> Bool -> Spec
-eventTests threadingMode hasNative = describe "Tests" $
-  forM_ (if hasNative then [False, True] else [True]) $ \poll -> describe (if poll then "Polling" else "Native") $ do
-    let ?timeInterval = if poll then 2*10^(6 :: Int) else 5*10^(5 :: Int)
-    forM_ [False, True] $ \recursive -> describe (if recursive then "Recursive" else "Non-recursive") $
-      forM_ [False, True] $ \nested -> describe (if nested then "In a subdirectory" else "Right here") $
-        makeTestFolder threadingMode poll recursive nested $ do
-          unless (nested || poll || isMac || isWin) $ it "deletes the watched directory" $ \(watchedDir, _f, getEvents, _clearEvents) -> do
-            removeDirectory watchedDir
-
-            pauseAndRetryOnExpectationFailure 3 $ getEvents >>= \case
-              [WatchedDirectoryRemoved {..}] | eventPath `equalFilePath` watchedDir && eventIsDirectory == IsDirectory -> return ()
-              events -> expectationFailure $ "Got wrong events: " <> show events
-
-          it "works with a new file" $ \(_watchedDir, f, getEvents, _clearEvents) -> do
-            openFile f AppendMode >>= hClose
-
-            pauseAndRetryOnExpectationFailure 3 $ getEvents >>= \events ->
-              if | nested && not recursive -> events `shouldBe` []
-                 | otherwise -> case events of
-                     [Added {..}] | eventPath `equalFilePath` f && eventIsDirectory == IsFile -> return ()
-                     _ -> expectationFailure $ "Got wrong events: " <> show events
-
-          it "works with a new directory" $ \(_watchedDir, f, getEvents, _clearEvents) -> do
-            createDirectory f
-
-            pauseAndRetryOnExpectationFailure 3 $ getEvents >>= \events ->
-              if | nested && not recursive -> events `shouldBe` []
-                 | otherwise -> case events of
-                     [Added {..}] | eventPath `equalFilePath` f && eventIsDirectory == IsDirectory -> return ()
-                     _ -> expectationFailure $ "Got wrong events: " <> show events
-
-          it "works with a deleted file" $ \(_watchedDir, f, getEvents, clearEvents) -> do
-            writeFile f "" >> clearEvents
-
-            removeFile f
-
-            pauseAndRetryOnExpectationFailure 3 $ getEvents >>= \events ->
-              if | nested && not recursive -> events `shouldBe` []
-                 | otherwise -> case events of
-                     [Removed {..}] | eventPath `equalFilePath` f && eventIsDirectory == IsFile -> return ()
-                     _ -> expectationFailure $ "Got wrong events: " <> show events
-
-          it "works with a deleted directory" $ \(_watchedDir, f, getEvents, clearEvents) -> do
-            createDirectory f >> clearEvents
-
-            removeDirectory f
-
-            pauseAndRetryOnExpectationFailure 3 $ getEvents >>= \events ->
-              if | nested && not recursive -> events `shouldBe` []
-                 | otherwise -> case events of
-                     [Removed {..}] | eventPath `equalFilePath` f && eventIsDirectory == IsDirectory -> return ()
-                     _ -> expectationFailure $ "Got wrong events: " <> show events
-
-          it "works with modified file attributes" $ \(_watchedDir, f, getEvents, clearEvents) -> do
-            writeFile f "" >> clearEvents
-
-            changeFileAttributes f
-
-            -- This test is disabled when polling because the PollManager only keeps track of
-            -- modification time, so it won't catch an unrelated file attribute change
-            pauseAndRetryOnExpectationFailure 3 $ getEvents >>= \events ->
-              if | poll -> return ()
-                 | nested && not recursive -> events `shouldBe` []
-                 | otherwise -> case events of
-#ifdef mingw32_HOST_OS
-                     [Modified {..}] | eventPath `equalFilePath` f && eventIsDirectory == IsFile -> return ()
-#else
-                     [ModifiedAttributes {..}] | eventPath `equalFilePath` f && eventIsDirectory == IsFile -> return ()
-#endif
-                     _ -> expectationFailure $ "Got wrong events: " <> show events
-
-          it "works with a modified file" $ \(_watchedDir, f, getEvents, clearEvents) -> do
-            writeFile f "" >> clearEvents
-
-            appendFile f "foo"
-
-            pauseAndRetryOnExpectationFailure 3 $ getEvents >>= \events ->
-              if | nested && not recursive -> events `shouldBe` []
-                 | otherwise -> case events of
-                     [Modified {..}] | eventPath `equalFilePath` f && eventIsDirectory == IsFile -> return ()
-                     _ -> expectationFailure $ "Got wrong events: " <> show events
