10d939a27cead496328384751a8a7163224d371b
thomasjm <tom@codedown.io>
2024-10-28 04:38:42 -0700

    Break out Util.hs module for Linux

--- src/System/FSNotify/Linux.hs
+++ src/System/FSNotify/Linux.hs
@@ -2,14 +2,10 @@
 -- Copyright (c) 2012 Mark Dittmer - http://www.markdittmer.org
 -- Developed for a Google Summer of Code project - http://gsoc2012.markdittmer.org
 --
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
@@ -21,23 +17,19 @@ module System.FSNotify.Linux (
 import Control.Concurrent.MVar
 import Control.Exception.Safe as E
 import Control.Monad
-import qualified Data.ByteString as BS
 import Data.Function
 import Data.Monoid
 import Data.String
 import Data.Time.Clock (UTCTime)
 import Data.Time.Clock.POSIX
-import qualified GHC.Foreign as F
-import GHC.IO.Encoding (getFileSystemEncoding)
 import Prelude hiding (FilePath)
-import System.Directory (canonicalizePath)
 import System.FSNotify.Find
+import System.FSNotify.Linux.Util
 import System.FSNotify.Listener
 import System.FSNotify.Types
-import System.FilePath (FilePath, (</>))
+import System.FilePath ((</>))
 import qualified System.INotify as INo
 import System.Posix.ByteString (RawFilePath)
-import System.Posix.Directory.ByteString (openDirStream, readDirStream, closeDirStream)
 import System.Posix.Files (getFileStatus, isDirectory, modificationTimeHiRes)
 
 
@@ -186,69 +178,3 @@ handleRecursiveEvent baseDir actPred callback watchStillExistsVar isRootWatchedD
   case event of
     INo.DeletedSelf | not isRootWatchedDir -> return ()
     _ -> handleInoEvent actPred callback baseDir watchStillExistsVar event
-
-
--- * Util
-
-canonicalizeRawDirPath :: RawFilePath -> IO RawFilePath
-canonicalizeRawDirPath p = fromRawFilePath p >>= canonicalizePath >>= toRawFilePath
-
--- | Same as </> but for RawFilePath
--- TODO: make sure this is correct or find in a library
-(<//>) :: RawFilePath -> RawFilePath -> RawFilePath
-x <//> y = x <> "/" <> y
-
-traverseAllDirs :: RawFilePath -> (RawFilePath -> IO ()) -> IO ()
-traverseAllDirs dir cb = traverseAll dir $ \subPath ->
-  -- TODO: wish we didn't need fromRawFilePath here
-  -- TODO: make sure this does the right thing with symlinks
-  fromRawFilePath subPath >>= getFileStatus >>= \case
-    (isDirectory -> True) -> cb subPath >> return True
-    _ -> return False
-
-traverseAll :: RawFilePath -> (RawFilePath -> IO Bool) -> IO ()
-traverseAll dir cb = bracket (openDirStream dir) closeDirStream $ \dirStream ->
-  fix $ \loop -> do
-    readDirStream dirStream >>= \case
-      x | BS.null x -> return ()
-      "." -> loop
-      ".." -> loop
-      subDir -> flip finally loop $ do
-        -- TODO: canonicalize?
-        let fullSubDir = dir <//> subDir
-        shouldRecurse <- cb fullSubDir
-        when shouldRecurse $ traverseAll fullSubDir cb
-
-boolToIsDirectory :: Bool -> EventIsDirectory
-boolToIsDirectory False = IsFile
-boolToIsDirectory True = IsDirectory
-
-toRawFilePath :: FilePath -> IO BS.ByteString
-toRawFilePath fp = do
-  enc <- getFileSystemEncoding
-  F.withCString enc fp BS.packCString
-
-fromRawFilePath :: BS.ByteString -> IO FilePath
-fromRawFilePath bs = do
-  enc <- getFileSystemEncoding
-  BS.useAsCString bs (F.peekCString enc)
-
-#if MIN_VERSION_hinotify(0, 3, 10)
-fromHinotifyPath :: BS.ByteString -> IO FilePath
-fromHinotifyPath = fromRawFilePath
-
-rawToHinotifyPath :: BS.ByteString -> IO BS.ByteString
-rawToHinotifyPath = return
-
-rawFromHinotifyPath :: BS.ByteString -> IO BS.ByteString
-rawFromHinotifyPath = return
-#else
-fromHinotifyPath :: FilePath -> IO FilePath
-fromHinotifyPath = return
-
-rawToHinotifyPath :: BS.ByteString -> IO FilePath
-rawToHinotifyPath = fromRawFilePath
-
-rawFromHinotifyPath :: FilePath -> IO BS.ByteString
-rawFromHinotifyPath = toRawFilePath
-#endif
---
+++ src/System/FSNotify/Linux/Util.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module System.FSNotify.Linux.Util (
+  canonicalizePath
+  , canonicalizeRawDirPath
+  , (<//>)
+  , traverseAllDirs
+
+  , boolToIsDirectory
+
+  , fromRawFilePath
+  , toRawFilePath
+
+  , fromHinotifyPath
+
+  , rawToHinotifyPath
+  , rawFromHinotifyPath
+  ) where
+
+import Control.Exception.Safe as E
+import Control.Monad
+import qualified Data.ByteString as BS
+import Data.Function
+import Data.Monoid
+import qualified GHC.Foreign as F
+import GHC.IO.Encoding (getFileSystemEncoding)
+import Prelude hiding (FilePath)
+import System.Directory (canonicalizePath)
+import System.FSNotify.Types
+import System.FilePath (FilePath)
+import System.Posix.ByteString (RawFilePath)
+import System.Posix.Directory.ByteString (openDirStream, readDirStream, closeDirStream)
+import System.Posix.Files (getFileStatus, isDirectory)
+
+
+canonicalizeRawDirPath :: RawFilePath -> IO RawFilePath
+canonicalizeRawDirPath p = fromRawFilePath p >>= canonicalizePath >>= toRawFilePath
+
+-- | Same as </> but for RawFilePath
+-- TODO: make sure this is correct or find in a library
+(<//>) :: RawFilePath -> RawFilePath -> RawFilePath
+x <//> y = x <> "/" <> y
+
+traverseAllDirs :: RawFilePath -> (RawFilePath -> IO ()) -> IO ()
+traverseAllDirs dir cb = traverseAll dir $ \subPath ->
+  -- TODO: wish we didn't need fromRawFilePath here
+  -- TODO: make sure this does the right thing with symlinks
+  fromRawFilePath subPath >>= getFileStatus >>= \case
+    (isDirectory -> True) -> cb subPath >> return True
+    _ -> return False
+
+traverseAll :: RawFilePath -> (RawFilePath -> IO Bool) -> IO ()
+traverseAll dir cb = bracket (openDirStream dir) closeDirStream $ \dirStream ->
+  fix $ \loop -> do
+    readDirStream dirStream >>= \case
+      x | BS.null x -> return ()
+      "." -> loop
+      ".." -> loop
+      subDir -> flip finally loop $ do
+        -- TODO: canonicalize?
+        let fullSubDir = dir <//> subDir
+        shouldRecurse <- cb fullSubDir
+        when shouldRecurse $ traverseAll fullSubDir cb
+
+boolToIsDirectory :: Bool -> EventIsDirectory
+boolToIsDirectory False = IsFile
+boolToIsDirectory True = IsDirectory
+
+toRawFilePath :: FilePath -> IO BS.ByteString
+toRawFilePath fp = do
+  enc <- getFileSystemEncoding
+  F.withCString enc fp BS.packCString
+
+fromRawFilePath :: BS.ByteString -> IO FilePath
+fromRawFilePath bs = do
+  enc <- getFileSystemEncoding
+  BS.useAsCString bs (F.peekCString enc)
+
+#if MIN_VERSION_hinotify(0, 3, 10)
+fromHinotifyPath :: BS.ByteString -> IO FilePath
+fromHinotifyPath = fromRawFilePath
+
+rawToHinotifyPath :: BS.ByteString -> IO BS.ByteString
+rawToHinotifyPath = return
+
+rawFromHinotifyPath :: BS.ByteString -> IO BS.ByteString
+rawFromHinotifyPath = return
+#else
+fromHinotifyPath :: FilePath -> IO FilePath
+fromHinotifyPath = return
+
+rawToHinotifyPath :: BS.ByteString -> IO FilePath
+rawToHinotifyPath = fromRawFilePath
+
+rawFromHinotifyPath :: FilePath -> IO BS.ByteString
+rawFromHinotifyPath = toRawFilePath
+#endif
